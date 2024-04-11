39d73ad9aff4985cd47a861ca8705d4f04c73421
Tom <tomd@airmail.cc>
2024-04-11 12:49:11 +1000

    Testnet docker test is working now. Had to change config.toml value to allow peers from the same network.

--- Test/testnet/bootstrap-entrypoint.sh
+++ Test/testnet/bootstrap-entrypoint.sh
@@ -3,7 +3,7 @@
 cp -r /tmp/cometbft-home /core/default-cometbft-home
 
 # Remove the persistent_peers from config.toml
-cd /core/default-cometbft-home/config && sed -i 's/persistent_peers = "538927808bbe633cc5dddfb1fe6fd7b3d62bd434@192.168.20.2:26656"/persistent_peers = ""/' config.toml
+cd /core/default-cometbft-home/config && sed -i 's/persistent_peers = "2a41ce3551fdf4dfdd2100b7ebb02538b7b7e996@192.168.20.30:26656"/persistent_peers = "ca0dc3d9b7e168fd3135c1da56b4abc2e55bd04e@139.180.178.149:26656"/' config.toml
 
 # Then the true entrypoint
 cd /core && /core/openmesh-core --config /core/conf/config.yml
