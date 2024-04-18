783ab9c5b0199b0bc44ff0d15d737eda4295e58d
dowdt <tomas.dowd@openmesh.network>
2024-04-18 16:12:40 +1000

    Moved testnet to docker-testnet

--- Test/docker-testnet/build-test.sh
+++ Test/docker-testnet/build-test.sh
@@ -7,9 +7,11 @@ export COMPOSE_HTTP_TIMEOUT=420
 if [ -d openmesh-core ]; then
   rm -f openmesh-core
 fi
+echo "building program..."
 # cd ../.. && GOOS=linux GARCH=amd64 go build -o openmesh-core
 cd ../.. && GOOS=linux GARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -o openmesh-core
 mv ./openmesh-core ./Test/testnet/openmesh-core
 
+echo "Done building program"
 # Build docker image
 cd Test/testnet && docker build -t openmesh-core-testnet:latest .
